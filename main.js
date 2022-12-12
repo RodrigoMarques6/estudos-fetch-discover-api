d304bf8079853c3fad5668158dc1520588d03f9e
RodrigoMarques <rodrigomarques328@hotmail.com>
2022-12-12 17:55:02 -0300

    fetch-discover-api

--- main.js
+++ main.js
@@ -27,4 +27,32 @@ function getUser() {
 }
 
 getUser()
+getUsers()
+
+// Agora vamos fazer um "POST", isto é, quero passar dados para a API. Neste caso, vamos incluir um novo usuário.
+
+function addUser(newUser) {
+    fetch(url, {
+        method: "POST",
+        // No method, pode receber "get" (que é o padrão, por isso não é necessário utilizar, caso seja get), pode ser POST também.
+        body: JSON.stringify(newUser),
+        // No body, colocamos o que será enviado para a url, isto é, o fetch vai pegar o que colocamos no body e enviar para dentro da url.
+        // Utilizamos a função JSON.stringify para que transformasse o conteúdo em JSON, e não em um objeto, tendo em vista que o HTML não mostra algo em formato/estrutura de objeto.
+        headers: {
+            "Content-type": "application/json;charset=UTF-8"
+            // O headers também é padrão, na verdade, essas 03 partes do código (method, body e headers) são conteúdos padrão.
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
+const newUser = {
+    name: "Olivia Zars",
+    avatar: "http://picsum.photos/200/300",
+    city: 'Rio do Sul'
+}
+
+addUser(newUser)
 
